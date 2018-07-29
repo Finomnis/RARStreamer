5f95189b7a29107eb3d4534e9d8d8de4a2b19643
Finomnis <martin.h.stumpf@gmail.com>
2018-07-29 23:49:53 +0200

    removed questionmark from password window

--- PasswordDialog.cpp
+++ PasswordDialog.cpp
@@ -7,12 +7,15 @@
 #include <iostream>
 
 PasswordDialog::PasswordDialog(QWidget *parent) :
-    QDialog(parent),
+    QDialog(parent, Qt::WindowTitleHint),
     ui(new Ui::PasswordDialog)
 {
     ui->setupUi(this);
 
+    // Make non-resizable
     setFixedSize(size());
+
+    // set focus to input field
     ui->password->setFocus();
 
     connect(this, SIGNAL(accepted()), this, SLOT(okButton()));
